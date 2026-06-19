39c59aee8ce1c708fec43f89253e50f08036af9f
is-moraes <153871018+is-moraes@users.noreply.github.com>
2026-06-19 00:45:34 -0300

    Migrate Firebase config to modular mode

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,8 @@
-// ===== FIREBASE CONFIG - Compat Mode =====
+// ===== FIREBASE CONFIG - Modular Mode =====
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import { getFirestore, collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc, query, where, orderBy, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDY0GvcQ6kYdoGL2eWnvCFPgb4M496h_3o",
   authDomain: "quadra-volei-sistema.firebaseapp.com",
@@ -8,7 +12,8 @@ const firebaseConfig = {
   appId: "1:332015795716:web:2cc619af2d4979d9bcbf51"
 };
 
-// Inicializar Firebase (compat mode)
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+// Exportar helpers do Firestore
+export { collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc, query, where, orderBy, serverTimestamp };
